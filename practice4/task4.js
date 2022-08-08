90724ec67b866e7a7cd876a286340e4d8f868371
Anton Chaika <91263261+kevandee@users.noreply.github.com>
2022-08-08 18:00:21 +0300

    Update task4.js

--- practice4/task4.js
+++ practice4/task4.js
@@ -1,25 +1,42 @@
 function parseDate(str) {
-    const reg = /(\d{1,2})\-(\d{1,2})\-(\d{4})/;
-    const arr = str.match(reg) || [];
-
-    if (arr) {
-        for (let i = arr.length - 1; i >= 0 ; i--) {
-            if (arr[i] < 10 && arr[i].length < 2) {
-                arr[i] = '0' + arr[i]; 
-            }
-        }
-
-        return [arr[3], arr[2], arr[1]].join('.');
+    const reg = /([\d{1,2}]+)\-([\d{1,2}]+)\-([\d{4}]+)/;
+    if (!reg.test(str)) {
+        return false;
     }
 
-    return null;
+    const arr = str.match(reg);
+    if ((arr[1] < 0 || arr[1] > 31)
+    || (arr[2] < 0 || arr[2] > 12)
+    || (arr[3] < 0)) {
+        return false;
+    }
+    
+    let obj = {};
+    let date = new Date(arr[3], arr[2] - 1, arr[1]);
+    if (date.getDate() <= 9) {
+        obj.date = '0' + date.getDate();
+    }
+    else {
+        obj.date = date.getDate();
+    }
+    if (date.getMonth() + 1 <= 9) {
+        obj.month = '0' + (date.getMonth() + 1)
+    }
+    else {
+        obj.month = date.getMonth() + 1;
+    }
+    obj.year = date.getFullYear();
+
+    return +obj.date == +arr[1] 
+        && +obj.month == +arr[2] 
+        && +obj.year == +arr[3];
 }
 
 function test() {
-    console.log(parseDate('25-07-2021')); // 2021.07.25
-    console.log(parseDate('25-7-2021')); // 2021.07.25
-    console.log(parseDate('1-1-2021')); // 2021.01.01
-    console.log(parseDate('2-12-1979')); // 1979.12.02
+    console.log(parseDate('25-07-2021')); 
+    console.log(parseDate('25-7-2021')); 
+    console.log(parseDate('07-1-2021')); 
+    console.log(parseDate('31-12-1979'));
 }
 
 console.log('--- Task 4 ---');
